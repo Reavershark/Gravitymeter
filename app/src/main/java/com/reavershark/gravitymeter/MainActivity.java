93ba8d7a19efa22a978b0f35c95c8bba20048c04
Reavershark <jonasty1999@gmail.com>
2016-09-06 21:51:50 +0200

    Added menu en methods for click events

--- app/src/main/java/com/reavershark/gravitymeter/MainActivity.java
+++ app/src/main/java/com/reavershark/gravitymeter/MainActivity.java
@@ -9,21 +9,21 @@ import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.Display;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class MainActivity extends Activity implements SensorEventListener {
+public class MainActivity extends Activity  implements SensorEventListener {
 
     //Vars
-    public float[] gravity = {0,0,0};
+    public float[] gravity = {0, 0, 0};
     public float pointerImgViewCenterX = 0;
     public float pointerImgViewCenterY = 0;
 
-    public TextView gravxTxtView;
-    public TextView gravyTxtView;
-    public TextView gravzTxtView;
-    public ImageView pointerImgView;
-    public ImageView centerImgView;
+    public TextView gravxTxtView, GravyTxtView, gravyTxtView, gravzTxtView;
+    public ImageView pointerImgView, centerImgView;
     public Handler mHandler;
     public int mInterval = 50; // in ms
 
@@ -46,18 +46,19 @@ public class MainActivity extends Activity implements SensorEventListener {
         pointerImgView.setImageAlpha(100);
         centerImgView.setImageResource(R.mipmap.circle);
 
-        SensorManager sensorManager=(SensorManager)getSystemService(SENSOR_SERVICE);
-        Sensor accel=sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        Sensor accel = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         sensorManager.registerListener(this, accel, SensorManager.SENSOR_DELAY_FASTEST);
     }
 
     @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy){}
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
 
     //Called every listened sensor update
     @Override
     public void onSensorChanged(SensorEvent event) {
-        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER && event.accuracy == SensorManager.SENSOR_STATUS_ACCURACY_MEDIUM) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER && event.accuracy == SensorManager.SENSOR_STATUS_ACCURACY_HIGH) {
             gravity[0] = event.values[0];
             gravity[1] = -event.values[1];
             gravity[2] = -event.values[2];
@@ -86,8 +87,7 @@ public class MainActivity extends Activity implements SensorEventListener {
         }
     };
 
-
-    private void startRepeater(){
+    private void startRepeater() {
         mRepeater.run();
     }
     /*
@@ -95,4 +95,26 @@ public class MainActivity extends Activity implements SensorEventListener {
         mHandler.removeCallbacks(mRepeater);
     }
     */
+
+
+    @Override //Create menu
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override //Called when an menu item is selected
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_action_about:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void aboutClicked(MenuItem item) {
+
+    }
 }
+
